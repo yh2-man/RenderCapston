6596abeda82fa4a808c843314ce80c37205d705a
yh2-man <vkdnjk1234@gmail.com>
2025-11-18 20:30:28 +0900

    force ssl

--- signaling-server/src/db/Db.js
+++ signaling-server/src/db/Db.js
@@ -6,7 +6,9 @@ const dbConfig = {
     database: process.env.DB_NAME || 'voice_chat',
     password: process.env.DB_PASSWORD || 'gkak1021',
     port: process.env.DB_PORT || 5432,
-    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    ssl: {
+        rejectUnauthorized: false
+    },
 };
 
 const pool = new Pool(dbConfig);
